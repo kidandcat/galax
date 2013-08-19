96cc1e52739adaab6dd0da944eecd7556f5ae849
kidandcat@gmail.com <kidandcat@gmail.com@1055ff28-82ed-6caf-5d38-650a968017f1>
2013-08-19 01:45:57 +0000

    info.php protegido

--- info.php
+++ info.php
@@ -1,3 +1,11 @@
 <?php
-echo phpinfo();
+session_start();
+if(isset($_SESSION['user'])){
+    if($_SESSION['user'] == 'kidandcat')
+        echo phpinfo();
+    else
+        echo '<b>Not enough level of authorization, access not allowed.</b>';
+}else{
+    echo '<b>Not enough level of authorization, access not allowed.</b>';
+}
 ?>
